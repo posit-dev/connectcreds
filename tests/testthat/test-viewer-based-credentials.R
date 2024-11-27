f53af8e63e6bad6b56d7c1e987ca94c5e9adbdfa
Aaron Jacobs <aaron.jacobs@posit.co>
2024-11-27 16:00:20 -0500

    Port over unit tests for viewer-based credentials from odbc/elmer.
    Signed-off-by: Aaron Jacobs <aaron.jacobs@posit.co>

---
+++ tests/testthat/test-viewer-based-credentials.R
@@ -0,0 +1,32 @@
+test_that("the session parameter is ignored when not on Connect", {
+  session <- structure(list(request = list()), class = "ShinySession")
+  expect_snapshot(. <- has_viewer_token(session))
+})
+
+test_that("missing viewer credentials generate errors on Connect", {
+  # Mock a Connect environment that *does not* support viewer-based credentials.
+  local_mocked_bindings(running_on_connect = function() TRUE)
+  session <- structure(list(request = list()), class = "ShinySession")
+  expect_snapshot(. <- has_viewer_token(session), error = TRUE)
+})
+
+test_that("token exchange requests to Connect look correct", {
+  # Mock a Connect environment that supports viewer-based credentials.
+  withr::local_envvar(
+    RSTUDIO_PRODUCT = "CONNECT",
+    CONNECT_SERVER = "localhost:3030",
+    CONNECT_API_KEY = "key"
+  )
+  local_mocked_responses(function(req) {
+    # Snapshot relevant fields of the outgoing request.
+    expect_snapshot(
+      list(url = req$url, headers = req$headers, body = req$body$data)
+    )
+    response_json(body = list(access_token = "token"))
+  })
+  session <- structure(
+    list(request = list(HTTP_POSIT_CONNECT_USER_SESSION_TOKEN = "user-token")),
+    class = "ShinySession"
+  )
+  expect_equal(connect_viewer_token(session), "token")
+})
